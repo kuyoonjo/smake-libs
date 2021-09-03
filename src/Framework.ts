e18b17f23f727c2e07b933a07e3c6dcbdbbc9ad6
Yu Chen <yu.chen@live.ie>
2021-09-03 12:07:03 +0800

    bugfix framework.

--- src/Framework.ts
+++ src/Framework.ts
@@ -4,7 +4,14 @@ export interface Framework {
   framework(t: LLVM): void;
 }
 
+const fwFuncs: {
+  [k: string]: (t: LLVM) => void;
+} = {};
+
 export function framework(Class: { new (): Framework }) {
-  const c = new Class();
-  return c.framework;
+  if (!fwFuncs[Class.name]) {
+    const c = new Class();
+    fwFuncs[Class.name] = (t: LLVM) => c.framework(t);
+  }
+  return fwFuncs[Class.name];
 }
